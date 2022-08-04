987bb6257cde774e0893bac33acebc88228da8a1
al-alec <yehadjialexis@gmail.com>
2022-08-04 14:06:16 +0000

    m

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,12 +4,13 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-10">
-            <div class="card">
+            <div class="card mt-4">
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
+                        <h2 class="mt-4">Credit Lyonnaise</h2>
 
                         <div class="form-group row">
                             <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
